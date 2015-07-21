044094be851c87fff967664f4132e3e0444f3fe7
Bruno Fabri <brunoferreirafabri@gmail.com>
2015-07-21 14:48:03 -0300

    assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These R file contains functions to:
+## 1 - Create a special kind of matrix that caches its inverse matrix;
+## 2 - Calculate the inverse matrix of this special matrix.
 
-## Write a short comment describing this function
+
+## Create a special matrix that caches its inverse matrix since it is calculated.
+## Receives as argument a normal matrix or a empty matrix by default.
+## Returns a list of functions to manipulate the matrix and the cached inverse matrix. 
+## These functions are described below:
+## set: Sets a new matrix and clear the cache
+## get: Gets the original matrix
+## setinverse: Sets the inverse matrix to cache
+## getinverse: Gets the inverse matrix from cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    
+    get <- function() x
+    
+    setinverse <- function(inverse) inv <<- inverse
+    
+    getinverse <- function() inv
+    
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Calculates the inverse matrix of the special "matrix" created with the above function.
+## However, it first checks to see if the inverse matrix has already been calculated. 
+## If so, it gets the inverse matrix from the cache and skips the computation. 
+## Otherwise, it calculates the inverse matrix of the data and sets the value of the 
+## inverse matrix in the cache via the setinverse function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$getinverse()
+    if (!is.null(inv)) {
+        message("getting inverse from cache")
+        return(inv)
+    }
+    
+    m <- x$get()
+    inv <- solve(m)
+    x$setinverse(inv)
+    
+    inv
 }
